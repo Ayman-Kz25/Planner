b5fb0652fa10e9bdd2c90e0ee8143b362c5a0b8d
Ayman-Kz25 <kamalakz786@gmail.com>
2026-07-21 23:11:06 +0500

    create login page functionality

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
-export default LoginPage
+  const { login, loginWithGoogle } = useAuth();
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    try {
+      setLoading(true);
+      await login(email, password);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      await loginWithGoogle();
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  return <></>;
+};
+export default LoginPage;
