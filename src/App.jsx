08e98818fe32699cca31b4b72cba6b9d899e053f
Ayman-Kz25 <kamalakz786@gmail.com>
2026-07-21 23:22:11 +0500

    create authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,59 @@ import AppLayout from "./components/layout/MainLayout";
 import DashboardPage from "./pages/DashboardPage";
 import InProgressPage from "./pages/InProgressPage";
 import CompletedPage from "./pages/CompletedPage";
-import CalendarPage from "./pages/CalendarPage"
+import CalendarPage from "./pages/CalendarPage";
 import CategoryPage from "./pages/CategoryPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
       <AppLayout>
         <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/in-progress" element={<InProgressPage />} />
-          <Route path="/completed" element={<CompletedPage />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/in-progress"
+            element={
+              <ProtectedRoute>
+                <InProgressPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/completed"
+            element={
+              <ProtectedRoute>
+                <CompletedPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <ProtectedRoute>
+                <CalendarPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/category/:category"
+            element={
+              <ProtectedRoute>
+                <CategoryPage />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </AppLayout>
     </BrowserRouter>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
-export default RegisterPage
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    try {
+      setLoading(true);
+      await signup(name, email, password);
+      navigate("/");
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return <></>;
+};
+export default RegisterPage;
